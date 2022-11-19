ee13e6486b277d94008bf15764e75900e504e26f
ahmedosama198 <ahmed.osama1982002@gmail.com>
2022-11-20 00:09:47 +0200

    It worked finally don;t touch!!

--- frontend/src/components/Navbar/OptionsList/ProfileLinks.js
+++ frontend/src/components/Navbar/OptionsList/ProfileLinks.js
@@ -1,28 +1,29 @@
-import React, { useState } from 'react'
-import styles from './styles/index.module.css'
+import React, { useState } from "react";
+import styles from "./styles/index.module.css";
 
-import useToken from '../../../useToken'
-import FormModel from '../../FormModel';
-import { Link } from 'react-router-dom';
+import useToken from "../../../useToken";
+import FormModel from "../../FormModel";
+import { Link } from "react-router-dom";
 
 export default function ProfileLinks() {
-    const [logoutModel, setLogoutModel] = useState(false);
+  const [logoutModel, setLogoutModel] = useState(false);
 
-    const { userdata } = useToken()
-    const { id, username } = userdata
-    console.log(userdata)
-    return (
-        <>
-            <ul className={styles.optionsList}>
-                <Link to={"/student/" + id}>{username}</Link>
-                <li onClick={() => setLogoutModel(true)}>Logout</li>
-            </ul>
+  const { userdata } = useToken();
+  console.log(userdata);
+  const { id, USERNAME } = userdata;
+  console.log(userdata);
+  return (
+    <>
+      <ul className={styles.optionsList}>
+        <Link to={"/student/" + id}>{USERNAME}</Link>
+        <li onClick={() => setLogoutModel(true)}>Logout</li>
+      </ul>
 
-            <FormModel
-                show={logoutModel}
-                onHide={() => setLogoutModel(false)}
-                type='logout'
-            />
-        </>
-    )
+      <FormModel
+        show={logoutModel}
+        onHide={() => setLogoutModel(false)}
+        type="logout"
+      />
+    </>
+  );
 }
