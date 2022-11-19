0114da54161ba90a858a99c563bd91d3908b4650
ahmedosama198 <ahmed.osama1982002@gmail.com>
2022-11-20 00:06:42 +0200

    HOOOOOOO

--- frontend/src/formsConfig.js
+++ frontend/src/formsConfig.js
@@ -1,61 +1,59 @@
-import * as Yup from 'yup';
-import YupPassword from 'yup-password'
-YupPassword(Yup) // extend yup
-
+import * as Yup from "yup";
+import YupPassword from "yup-password";
+YupPassword(Yup); // extend yup
 
 /* inital values */
 export const loginInitialValues = {
-    email: '',
-    password: ''
-}
+  email: "",
+  password: "",
+};
 
 export const signupInitialValues = {
-    username: '',
-    firstname: '',
-    lastname: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
-}
+  username: "",
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 /* validation schemas */
 export const LoginSchema = Yup.object().shape({
-    /* email validation */
-    email: Yup.string()
-        .email('please enter a valid email')
-        .required('required'),
+  /* email validation */
+  email: Yup.string().email("please enter a valid email").required("required"),
 
-    /* password validation */
-    password: Yup.string()
-        .password()
-        .min(8, "at least 8 characters")
-        .max(128, "at most 128 characters")
-        .minLowercase(1, "at lease 1 lower case letter")
-        .minUppercase(1, "at lease 1 upper case letter")
-        .minNumbers(1, "at least 1 number")
-        .minSymbols(1, "at lease 1 symbol")
-        .required('required'),
-
+  /* password validation */
+  password: Yup.string()
+    .password()
+    .min(8, "at least 8 characters")
+    .max(128, "at most 128 characters")
+    .minLowercase(1, "at lease 1 lower case letter")
+    .minUppercase(1, "at lease 1 upper case letter")
+    .minNumbers(1, "at least 1 number")
+    .minSymbols(1, "at lease 1 symbol")
+    .required("required"),
 });
 
 export const SignupSchema = LoginSchema.shape({
-    /* first name */
-    firstName: Yup.string()
-        .min(3, 'at least 3 characters')
-        .max(32, 'at most 32 characters')
-        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field ")
-        .required('required'),
+  /* first name */
+  firstName: Yup.string()
+    .min(3, "at least 3 characters")
+    .max(32, "at most 32 characters")
+    .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field ")
+    .required("required"),
 
-    lastName: Yup.string()
-        .min(3, 'at least 3 characters')
-        .max(32, 'at most 32 characters')
-        .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field ")
-        .required('required'),
+  lastName: Yup.string()
+    .min(3, "at least 3 characters")
+    .max(32, "at most 32 characters")
+    .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field ")
+    .required("required"),
 
-    username: Yup.string()
-        .min(3, 'at least 3 characters')
-        .max(32, 'at most 32 characters')
-        .required('required'),
-    confirmPassword: Yup.string()
-        .oneOf([Yup.ref('password'), null], 'Passwords must match')
-})
+  username: Yup.string()
+    .min(3, "at least 3 characters")
+    .max(32, "at most 32 characters")
+    .required("required"),
+  confirmPassword: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Passwords must match"
+  ),
+});
