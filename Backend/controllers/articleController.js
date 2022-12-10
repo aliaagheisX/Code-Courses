6bfd37025bbec896e48fa6604c9cb391777fe840
Ahmed Abdelatty <ahmedradalla2001@gmail.com>
2022-12-10 11:19:12 +0200

    زز

--- Backend/controllers/articleController.js
+++ Backend/controllers/articleController.js
@@ -1,5 +1,61 @@
 const articleRepo = require('../repositories/articleRepository');
+const userRepo = require('../repositories/userRepository');
 
 module.exports = {
-    
+	getAllArticles: async (req, res) => {
+		try {
+			let articles = await articleRepo.getAllArticles();
+			if (!articles.length) {
+				return res
+				.status(404)
+				.send({ message: "Looks like you have no articles" });
+			}
+			return res.status(200).send({ articles: articles });
+		} catch (err) {
+			return res      
+				.status(500)
+				.send({ message: "Internal server error getting all articles" + err });
+		}
+	},
+	getArticleById: async (req, res) => {
+		try {
+      let id = parseInt(req.params.a_id);
+      let article = await articleRepo.getArticleById(id);
+      if (!article) {
+        return res
+          .status(404)
+          .send({ message: "Article not found" });
+      }
+      return res.status(200).send({ article: article });
+    } catch (err) {
+      return res
+        .status(500)
+        .send({ message: "Internal server error getting article by id" + err });
+    }
+	},
+	getArticlesByِAuthorUsername: async (req, res) => {
+		try {
+			let username = req.params.username;
+			let author = await userRepo.getUserByName(username);
+			if (!author) {
+				return res
+					.status(404)
+					.send({ message: "Author "});
+			}
+			let articles = articleRepo.getArticlesByAuthorUsername(username);
+			if (!articles.length) {
+				return res
+					.status(404)
+					.send({ message: "Th"})
+			}
+		} catch (err) {
+
+		}
+	},
+	getArticlesByCourseId: async (req, res) => {
+		
+	},
+	getArticlesByTopicId: async (req, res) => {
+
+	}
 }
--- Backend/routes/articles.js
+++ Backend/routes/articles.js
@@ -2,3 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const articleController = require('../controllers/articleController');
+
+router.get('/', articleController.getAllArticles);
+router.get('/:a_id', articleController.getArticleById);
+router.get('/:username', articleController.getArticlesByAuthorUsername);
+router.get('/:c_id', articleController.getArticlesByCourseId);
+router.get('/:t_id', articleController.getArticlesByTopicId);
