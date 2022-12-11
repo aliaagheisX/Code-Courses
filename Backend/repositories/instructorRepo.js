5bd9839dd3f10180985cac68b7b3f1d8e4fa67a8
Ahmed Abdelatty <ahmedradalla2001@gmail.com>
2022-12-11 11:46:25 +0200

    ز

---
+++ Backend/repositories/instructorRepo.js
@@ -0,0 +1,9 @@
+const { DBConnection } = require('../config/database');
+
+module.exports = {
+    getAllInstructors: () => {
+        return new Promise((resolve, reject) => {
+            let queryString = `SELECT `
+        })
+    }
+}
