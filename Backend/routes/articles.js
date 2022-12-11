f1c10358278ed3c1dd1b4d7c5111efb74bc1a98c
AhmedOsama198 <ahmed.osama1982002@gmail.comm>
2022-12-11 11:05:05 +0200

    swagger for article create

--- Backend/routes/articles.js
+++ Backend/routes/articles.js
@@ -12,7 +12,75 @@ router.get('/getbytopicid/:t_id', articleController.getArticlesByTopicId);
 
 router.delete('/', [admin], articleController.deleteAllArticles);
 router.delete('/:a_id', [admin], articleController.deleteArticleById);
-
+/**
+ * @swagger
+ * /articles/create:
+ *  post:
+ *      summary: Creates a new article
+ *      tags: [articles]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/article'
+ *      parameters:
+ *      - in: path
+ *        name: instructor_id
+ *        schema:
+ *            type: integer
+ *        required: true
+ *        description: The article writer 
+*      - in: path
+ *        name: title
+ *        schema:
+ *            type: string
+ *        required: true
+ *        description: element title
+ *      - in: path
+ *        name: description
+ *        schema:
+ *            type: string
+ *        required: true
+ *        description: element description
+ *      - in: path
+ *        name: body
+ *        schema:
+ *            type: string
+ *        required: true
+ *        description: article body
+ *      responses:
+ *          200:
+ *              description: Success message
+ *              
+ *          403:
+ *              description: validation error
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              message:
+ *                                  type: string 
+ *                                  description: validation error + error
+ *          401:
+ *              description: Unauthorized
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  description: Unauthorized + reason
+ *          500:
+ *              description: error with query
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  description: internal server error + error
+ */
 router.post('/create', articleController.createArticle);
 
 module.exports = router;
