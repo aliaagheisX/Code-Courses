916c7dc485e153c50849f4ce32fa67ba8aaed244
aliaa gheis <aliaagheis@gmail.com>
2022-12-14 06:39:14 +0200

    end show users

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,6 +3,7 @@ const api = {
     signup: 'http://localhost:4000/auth/register',
     logout: '',
     deleteMe: 'http://localhost:4000/users/delete/me',
+    getStudents: 'http://localhost:4000/students',
     editProfile: (username) => `http://localhost:4000/users/${username}/edit`,
     user: (id) => `http://localhost:4000/users/${id}`,
     student: (id) => `http://localhost:4000/students/${id}`,
