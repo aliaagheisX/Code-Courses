363a5f0998d1fe7862e733a55b0814b0a47b4f35
Ahmed Abdelatty <ahmedradalla2001@gmail.com>
2022-12-23 19:45:42 +0200

    review api

--- Backend/permissions/coursePermissions.js
+++ Backend/permissions/coursePermissions.js
@@ -17,7 +17,14 @@ module.exports = {
 		let id = req.user.ID;
 		let c_id = req.params.c_id;
 		if (isInstructor(id)) {
-			let course = await courseRepo.getCourseById(c_id);
+			let course = null;
+			try {
+				course = await courseRepo.getCourseById(c_id);
+			} catch (err) {
+				return res
+					.status(500)
+					.send({ message: "Internal server error getting course by id " + err });
+			}
 			if (course.INSTRUCTORID === id) {
 				next();
 			} else {
@@ -31,4 +38,32 @@ module.exports = {
 				.send({ message: "Unauthorized. Must be instructor" })
 		}
 	},
+	canEditReview: async (req, res, next) => {
+		let u_id = parseInt(req.params.u_id);
+		let userID = req.user.ID;
+		if (u_id === userID) {
+			next();
+		}
+		return res
+			.status(401)
+			.send({ message: "Unauthorized. This isn't your review" });
+	},
+	canReview: async (req, res, next) => {
+		let u_id = req.user.ID;
+		let c_id = parseInt(req.params.c_id);
+		let is_enrolled = null;
+		try {
+			is_enrolled = await courseRepo.getUserEnrolled(u_id, c_id);
+		} catch(err) {
+			return res
+				.status(500)
+				.send({ message: "Internal server error getting user enrolled " + err });
+		}
+		if (!is_enrolled) {
+			return res
+				.status(401)
+				.send({ message: "Unauthorized. You are not enrolled in this course" });
+		}
+		next();
+	}
 }
