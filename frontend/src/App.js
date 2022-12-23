94eec3b79e694023b5e1644ce072795b65f57745
Menna-Ahmed7 <mennaahmed0701@gmail.com>
2022-12-23 23:33:34 +0200

    Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ export default function App() {
                 <div style={{ margin: "0 auto", width: "85%", minWidth: "250px", padding: "4rem 0" }}>
                     <Outlet />
                 </div>
+            
+
             </div>
         </>
     )
