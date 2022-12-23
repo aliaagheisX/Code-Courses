fbd95a0562d9fb459a4b1381de6ed53d73262112
AhmedOsama198 <ahmed.osama1982002@gmail.comm>
2022-12-23 10:06:22 +0200

    getQuizzes by Instructor

--- Backend/routes/quizzes.js
+++ Backend/routes/quizzes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const quizController = require("../controllers/quizController");
 
 router.post("/create", quizController.postQuiz);
+router.get("/getByInstructor", quizController.getQuizzesByInstructor);
 
 module.exports = router;
