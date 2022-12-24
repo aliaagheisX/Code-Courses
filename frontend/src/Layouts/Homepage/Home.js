d153691053d7c48718112fb4fc5c7eef0170a7a0
Menna-Ahmed7 <mennaahmed0701@gmail.com>
2022-12-24 16:02:47 +0200

    home screen

--- frontend/src/Layouts/Homepage/Home.js
+++ frontend/src/Layouts/Homepage/Home.js
@@ -1,26 +1,45 @@
 import Resource from '../../Resource'
 import ArticleComponent from '../../components/ArticleComponent'
+import CourseComponent from '../../components/CourseComponent'
 import api from '../../api'
 import CustomCarsoul from '../../components/CustomCarsoul'
+import Courses from '../Courses'
 
 const Home = () => {
   return (<div>
+  <section>
+  <h2 >Articles</h2>
+     <Resource
+        path={api.getArticles}
+        render={({ items: { articles } }) => (
+          <div className='elementCont' >
+            {
+              articles.map((article) => (
+                <ArticleComponent article={article} key={article.ID} />
+              ))
+            }
+          </div>
+        )}
+        ErrorComp={<></>}
+      />
+      </section>
+      <section>
+  <h2>Courses</h2>
     <Resource
-      path={api.getArticles}
-      render={({ items: { articles } }) => (
+                path={api.getAllCourses}
+                render={({ items: { courses } }) => (
+                    <div className='elementCont' >
+                        {
+                            courses.map((course) => (
+                                <CourseComponent course={course} key={course.ID} />
+                            ))
+                        }
+                    </div>
+                )}
 
-        <CustomCarsoul
-          items={
-            articles.map((article) => (
-              <ArticleComponent article={article} key={article.ID} />
-            ))
-          }
-        />
-
-      )}
-      ErrorComp={<></>}
-    />
-
+                ErrorComp={<></>}
+            />
+      </section>
 
   </div>);
 }
